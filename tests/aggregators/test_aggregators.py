afef1dedae2883b668232b7d023cced5415fc420
Cliff Wulfman <cwulfman@gmail.com>
2021-01-09 17:14:58 -0500

    tests work

--- tests/aggregators/test_aggregators.py
+++ tests/aggregators/test_aggregators.py
@@ -11,7 +11,7 @@ import os
 def test_include():
     factory = AggregatorFactory()
     input_dir = os.path.dirname(__file__)
-    subject = factory.aggregator_for("DDHI", input_dir)
+    subject = factory.aggregator_for("DDHI", input_dir, '/tmp')
     interview = Interview()
     interview.read(os.path.join(os.path.dirname(__file__), "test1.tei.xml"))
     subject.include(interview)
@@ -25,4 +25,4 @@ def test_include():
     assert subject.places[0].coordinates == "43.702222 -72.206111"
 
     output = subject.formatted_interview(subject.interviews[0])
-    assert(etree.tostring(output)) == b'<interview><identifier>dvp_test1</identifier><title>test transcript</title><primary_audio_URI/><interview_body>foo</interview_body><participants><participant><name/><role/></participant><participant><name/><role/></participant></participants></interview>'
+    assert(etree.tostring(output)) == b'<interview>\n  <identifier>dvp_test1</identifier>\n  <title>test transcript</title>\n  <primary_audio_URI/>\n  <interview_body>foo</interview_body>\n  <participants>\n    <participant>\n      <name/>\n      <role/>\n    </participant>\n    <participant>\n      <name/>\n      <role/>\n    </participant>\n  </participants>\n  <named_persons>\n    <name>Emily Burack</name>\n    <id type="DDHI"/>\n    <name>Beirne [pronounced BUR-nee] Lovely</name>\n    <id type="DDHI"/>\n  </named_persons>\n  <named_places>\n    <place>Hanover, New Hampshire</place>\n    <id type="DDHI">Q131908</id>\n    <place>Boston</place>\n    <id type="DDHI"/>\n  </named_places>\n  <named_events/>\n  <named_orgs/>\n</interview>'
--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from ddhi_aggregator.aggregators.aggregators import Aggregator
-from ddhi_encoder.interview import Interview
+from ddhi_aggregator.aggregators.aggregators import AggregatorFactory
 
-from ddhi_aggregator.aggregate import aggregate_tei
 import os
 
 __author__ = "Cliff Wulfman"
@@ -13,10 +11,7 @@ __license__ = "mit"
 
 def test_aggregate():
     in_dir = os.path.join(os.path.dirname(__file__), "interviews")
-    aggregator = Aggregator()
-    for f in os.listdir(in_dir):
-        if f.endswith(".tei.xml"):
-            interview = Interview()
-            interview.read(os.path.join(in_dir, f))
-            aggregator.include(interview)
+    factory = AggregatorFactory()
+    aggregator = factory.aggregator_for("DDHI", in_dir, '/tmp')
+    aggregator.aggregate()
     assert len(aggregator.interviews) == 3
